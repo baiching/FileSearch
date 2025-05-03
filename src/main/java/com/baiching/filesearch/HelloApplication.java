158406fa1440bf47a650e9a545e0538ba7b1268c
baiching <bachmarma@gmail.com>
2025-05-03 22:18:12 +0200

    added hotkey

--- src/main/java/com/baiching/filesearch/HelloApplication.java
+++ src/main/java/com/baiching/filesearch/HelloApplication.java
@@ -1,23 +1,53 @@
 package com.baiching.filesearch;
 
+import com.baiching.filesearch.utils.HotKeyManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private static Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage = stage;
+        setupUI();
+        HotKeyManager.registerHotKey(this::toggleWindow);
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    // Toggle window visibility (called when Ctrl+Space is pressed)
+    private void toggleWindow() {
+        if (primaryStage.isShowing()) {
+            primaryStage.hide();
+        }
+        else {
+            primaryStage.show();
+            primaryStage.toFront(); // Bring to front
+        }
+    }
+
+    private void setupUI() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("hello-view.fxml")
+        );
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.UNDECORATED); // Borderless window
+        primaryStage.setTitle("Spotlight Clone");
+        primaryStage.hide(); // Start hidden (toggle via hotkey)
+    }
+
+    @Override
+    public void stop() {
+        HotKeyManager.unregisterHotKey();
     }
 }
--- src/main/java/com/baiching/filesearch/utils/HotKeyManager.java
+++ src/main/java/com/baiching/filesearch/utils/HotKeyManager.java
@@ -1,7 +1,46 @@
 package com.baiching.filesearch.utils;
 
+import com.sun.jna.platform.win32.User32;
+import javafx.application.Platform;
+
 public class HotKeyManager {
     /*
     * Global Hot key(Jna/JNI)
      */
+    private static final int MOD_CONTROL = 0x0002; // Ctrl key
+    private static final int VK_SPACE = 0x20;
+    private static final int HOTKEY_ID = 1;
+
+    public static void registerHotKey(Runnable toggleAction) {
+        // Register Ctrl+Space hotkey
+        System.out.println("Registering hotkey");
+        boolean success = User32.INSTANCE.RegisterHotKey(
+                null, // use NULL to associate with current thread
+                HOTKEY_ID,
+                MOD_CONTROL,
+                VK_SPACE
+        );
+        System.out.println("Heyeeee");
+        if (!success) {
+            System.err.println("Failed to register hotkey");
+            return;
+        }
+        System.out.println(success);
+
+        // start the thread that will handle the hotkey
+        new Thread(() -> {
+            User32.MSG msg = new User32.MSG();
+            while (User32.INSTANCE.GetMessage(msg, null, 0, 0) != 0) {
+                if (msg.message == User32.WM_HOTKEY) {
+                    Platform.runLater(toggleAction);
+                }
+            }
+
+        }, "HotkeyListener").start();
+    }
+
+    // Call this on app shutdown
+    public static void unregisterHotKey() {
+        com.baiching.filesearch.utils.User32.INSTANCE.UnregisterHotKey(null, HOTKEY_ID);
+    }
 }
