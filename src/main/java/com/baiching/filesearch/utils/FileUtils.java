3bb2cb5bc3f3097fd94ccd71af2f5f9c9fb52d0f
baiching <bachmarma@gmail.com>
2025-05-07 23:41:40 +0200

    added listFilesUsingFilesList, which returns file names in a directory

--- src/main/java/com/baiching/filesearch/utils/FileUtils.java
+++ src/main/java/com/baiching/filesearch/utils/FileUtils.java
@@ -1,7 +1,24 @@
 package com.baiching.filesearch.utils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class FileUtils {
     /*
     * File path helpers
      */
+    public Set<String> listFilesUsingFilesList(String dir) throws IOException {
+        try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+            return stream
+                    .filter(file -> !Files.isDirectory(file))
+                    .map(Path::getFileName)
+                    .map(Path::toString)
+                    .collect(Collectors.toSet());
+        }
+    }
 }
