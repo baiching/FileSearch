f4ea106ae78f4d258a2393a6e520ed9ab7e93263
baiching <bachmarma@gmail.com>
2025-05-10 13:22:12 +0200

    added db class which will save directory paths to sqlite and provide fast search option

---
+++ src/main/java/com/baiching/filesearch/utils/DBOperations.java
@@ -0,0 +1,69 @@
+package com.baiching.filesearch.utils;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBOperations {
+    public void createDatabase() throws SQLException {
+        try (Connection conn = DatabaseConnection.getConnection()) {
+            Statement stmt = conn.createStatement();
+            stmt.execute("CREATE TABLE IF NOT EXISTS paths (path TEXT PRIMARY KEY)");
+        }
+    }
+
+    public void writePathToDB(String dir) throws SQLException, IOException {
+        Path startDir = Paths.get(dir);
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement("INSERT OR IGNORE INTO paths VALUES (?)")) {
+            Files.walkFileTree(startDir, new SimpleFileVisitor<>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+                    insertPath(dir, pstmt);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                    insertPath(file, pstmt);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                    return (exc instanceof AccessDeniedException)
+                            ? FileVisitResult.SKIP_SUBTREE
+                            : FileVisitResult.CONTINUE;
+                }
+            });
+        }
+    }
+    private void insertPath(Path path, PreparedStatement ps) {
+        try {
+            ps.setString(1, path.toAbsolutePath().toString());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> searchPaths(String keyword) throws SQLException {
+        List<String> results = new ArrayList<>();
+
+        try (Connection conn = DatabaseConnection.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement("SELECT path FROM paths WHERE path LIKE ?")) {
+            pstmt.setString(1, "%" + keyword +"%");
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                results.add(rs.getString("path"));
+            }
+
+        }
+
+        return results;
+    }
+}
